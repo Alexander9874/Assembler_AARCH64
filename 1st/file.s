d1298012bf15703540db379bf7ce98263c85f6be
sereda.ab <sashasereda93@gmail.com>
2022-03-08 20:52:42 +0300

    1st lab is probably done

--- 1st/file.s
+++ 1st/file.s
@@ -1,26 +1,29 @@
 //
+//	25 - 7
+//
 //	      a * c   d * b   c ^ 2
 //	res = ----- + ----- - -----
 //	        b       e     a * d
 //
 //	size of all variables is 16
 //
-
+//	vars are from -32767 to 32767
+//
 	.arch 	armv8-a
 	.data
 	.align	3
 res:
 	.skip	8
 a:
-	.short	-1
+	.short	1
 b:
-	.short	4
+	.short	1
 c:
-	.short	4
+	.short	8
 d:
-	.short	4
+	.short	1
 e:
-	.short	4
+	.short	1
 	.text
 	.align	2
 	.global	_start
@@ -40,14 +43,14 @@ _start:
 	sdiv	w5, w5, w1		// w5 is (a*c)/b
 	mul	w6, w3, w1		// w6 is d*b
 	sdiv	w6, w6, w4		// w5 is (d*b)/e
-	add	w5, w5, w6, sxtw	// x5 is ((a*c)/b)+((d*b)/e)
+	add	w5, w5, w6		// x5 is ((a*c)/b)+((d*b)/e)
 	mul	w6, w2, w2		// w6 is c*c
 	sdiv	w6, w6, w0		// w6 is c*c/a
 	sdiv	w6, w6, w3		// w6 is (c*c)/(a*d)
-	add	x5, x5, x6		// res
+	sub	x5, x5, x6		// res
 	adr	x0, res
-	str	x8, [x0]
+	str	x5, [x0]
 	mov	x0, #0
-	mov	x8, #93
+	mov	x5, #93
 	svc	#0
 	.size	_start, .-_start
